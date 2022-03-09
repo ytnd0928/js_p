02a80d053ad5a417c5652e4598169c75ce66543c
nodachan <72251931+ytnd0928@users.noreply.github.com>
2022-03-09 21:05:49 +0900

    closure

---
+++ closure.js
@@ -0,0 +1,26 @@
+function createClosure () {
+
+    const value = 'myClosureValue';
+
+    function myClosure () {
+        //valueはClosureの外ではあるが、myclosureと同じcreateClosureの関数スコープに居るので束縛する。
+
+        console.log(value)
+    }
+
+    return myClosure
+}
+
+const closure = createClosure()
+closure()
+
+/** 実行の流れ
+createclosure関数が呼ばれる
+        　↓
+createClosure関数ではmyClosure関数の参照を返す
+        　↓
+↑で受け取った参照をclosure変数に格納する
+　　　　　　↓
+closure関数(実態はmyClkosure関数)を呼び出す
+
+**/
